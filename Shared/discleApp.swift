dd9e2e58760643ad6368073e35c15f40edf94f98
Lucas Cron <lu@LuMac.local>
2022-03-10 20:32:11 -0500

    base files from xcode app

---
+++ Shared/discleApp.swift
@@ -0,0 +1,17 @@
+//
+//  discleApp.swift
+//  Shared
+//
+//  Created by Lucas Cron on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct discleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
